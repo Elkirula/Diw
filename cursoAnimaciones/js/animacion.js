8b13d34f61e46243a6eec80b2e1d41ae28c33ec3
Elkirula <elkirulas@gmail.com>
2023-11-14 23:18:06 +0100

    annimaciones

---
+++ cursoAnimaciones/js/animacion.js
@@ -0,0 +1,17 @@
+window.onload = function load(){
+
+    let chinchenta1 = document.getElementById('chinchenta1');
+    chinchenta1.classList.add('chinchetaAnimada');
+  
+    let chinchenta2 = document.getElementById('chinchenta2');
+    chinchenta2.classList.add('chinchetaAnimada2');
+  
+    let chinchenta3 = document.getElementById('chinchenta3');
+    chinchenta3.classList.add('chinchetaAnimada3');
+  
+    let articulo1 = document.getElementsByClassName('articulo1');
+    articulo1.classList.add('chinchetaAnimada');
+  
+    const articulo3 = document.getElementsByClassName('articulo3');
+    articulo3.classList.add('chinchetaAnimada');
+  }
